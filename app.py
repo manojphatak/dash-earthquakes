59e75b4e502dea2d6181da3ce1afb3a8acd0123b
Manoj Phatak <>
2018-04-24 13:42:26 +0530

    removed unnecessory controls

--- app.py
+++ app.py
@@ -10,35 +10,7 @@ import plotly.plotly as py
 from flask import Flask, json
 from dash import Dash
 from dash.dependencies import Input, Output
-from dotenv import load_dotenv
 
-external_js = [
-    # jQuery, DataTables, script to initialize DataTables
-    'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-    '//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js',
-    'https://codepen.io/jackdbd/pen/bROVgV.js',
-]
-
-external_css = [
-    # dash stylesheet
-    'https://codepen.io/chriddyp/pen/bWLwgP.css',
-    'https://fonts.googleapis.com/css?family=Raleway',
-    # 'https://fonts.googleapis.com/css?family=Lobster',
-    '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    '//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css',
-]
-
-
-try:
-    # the app is on Heroku
-    os.environ['DYNO']
-    debug = False
-    # google analytics with my tracking ID
-    external_js.append('https://codepen.io/jackdbd/pen/NgmpzR.js')
-except KeyError:
-    debug = True
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
 
 py.sign_in(os.environ['PLOTLY_USERNAME'], os.environ['PLOTLY_API_KEY'])
 
@@ -48,27 +20,8 @@ url = '{}{}'.format(usgs, geoJsonFeed)
 req = requests.get(url)
 data = json.loads(req.text)
 
-# local development
-# with open('4.5_month.geojson') as data_file:
-#     data = json.load(data_file)
 
 mapbox_access_token = os.environ.get('MAPBOX_ACCESS_TOKEN', 'mapbox-token')
-
-# http://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=5
-colorscale_magnitude = [
-    [0, '#ffffb2'],
-    [0.25, '#fecc5c'],
-    [0.5, '#fd8d3c'],
-    [0.75, '#f03b20'],
-    [1, '#bd0026'],
-]
-
-# http://colorbrewer2.org/#type=sequential&scheme=Greys&n=3
-colorscale_depth = [
-    [0, '#f0f0f0'],
-    [0.5, '#bdbdbd'],
-    [0.1, '#636363'],
-]
 
 
 theme = {
@@ -152,16 +105,6 @@ def create_header(some_string):
     return header
 
 
-regions = {
-    'world': {'lat': 0, 'lon': 0, 'zoom': 1},
-    'europe': {'lat': 50, 'lon': 0, 'zoom': 3},
-    'north_america': {'lat': 40, 'lon': -100, 'zoom': 2},
-    'south_america': {'lat': -15, 'lon': -60, 'zoom': 2},
-    'africa': {'lat': 0, 'lon': 20, 'zoom': 2},
-    'asia': {'lat': 30, 'lon': 100, 'zoom': 2},
-    'oceania': {'lat': -10, 'lon': 130, 'zoom': 2},
-}
-
 app_name = 'Dash Earthquakes'
 server = Flask(app_name)
 server.secret_key = os.environ.get('SECRET_KEY', 'default-secret-key')
@@ -180,6 +123,23 @@ app.layout = html.Div(
     style={'font-family': theme['font-family']}
 )
 
+
+external_js = [
+    # jQuery, DataTables, script to initialize DataTables
+    'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+    '//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js',
+    'https://codepen.io/jackdbd/pen/bROVgV.js',
+]
+
+external_css = [
+    # dash stylesheet
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    'https://fonts.googleapis.com/css?family=Raleway',
+    # 'https://fonts.googleapis.com/css?family=Lobster',
+    '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    '//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css',
+]
+
 for js in external_js:
     app.scripts.append_script({'external_url': js})
 
@@ -189,4 +149,4 @@ for css in external_css:
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run_server(debug=debug,  port=port, threaded=True)
+    app.run_server(debug=True,  port=port, threaded=True)
